678f086c87ab95345daf1f61c8a723ea94deca39
Abderrazak LAANAYA <laanayabdrzak@gmail.com>
2024-03-03 00:25:12 +0100

    Initial Commit

---
+++ SwiftUIMapKit/SwiftUIMapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapKitApp.swift
+//  SwiftUIMapKit
+//
+//  Created by LAANAYA Abderrazak on 3/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
